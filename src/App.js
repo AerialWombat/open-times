f72a3d855dcb657a1950de8501759167645cba43
AerialWombat <adrianzhan@gmail.com>
2019-04-08 17:19:15 -0400

    refactored router switch component to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
+import Navbar from "./components/Navbar/navbar";
 import Register from "./components/Register/register";
+import Login from "./components/Login/login";
+import { Switch, Route } from "react-router-dom";
 
-import "./App.css";
+import "./App.scss";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
-        <Register />
+        <Navbar />
+        <main>
+          <Switch>
+            <Route exact path="/" />
+            <Route path="/user/register" component={Register} />>
+            <Route path="/user/login" component={Login} />
+          </Switch>
+        </main>
       </div>
     );
   }
