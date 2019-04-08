ee740f05fba8fac62c6d85d9c9dc2b55869b71ed
AerialWombat <adrianzhan@gmail.com>
2019-04-08 17:31:31 -0400

    Added state to login/register forms

---
+++ src/components/Login/Login.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { IconContext } from "react-icons";
+import { FaSignInAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+import styles from "./login.module.scss";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: null,
+      password: null,
+      errorMessages: []
+    };
+  }
+
+  onEmailChange = event => {
+    this.setState({ ...this.state, email: event.target.value });
+  };
+
+  onPasswordChange = event => {
+    this.setState({ ...this.state, password: event.target.value });
+  };
+
+  onLoginSubmit = event => {
+    console.log(this.state);
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <form className={styles.container} onSubmit={this.onLoginSubmit}>
+        <h1 className={styles.title}>
+          <IconContext.Provider
+            value={{ color: "black", className: "global-class-name" }}
+          >
+            <FaSignInAlt />
+          </IconContext.Provider>
+          Login
+        </h1>
+        <p>Errors here</p>
+        <div className={styles.inputWrapper}>
+          <label for="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter email"
+            required
+            onChange={this.onEmailChange}
+          />
+        </div>
+        <div className={styles.inputWrapper}>
+          <label for="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter password"
+            required
+            onChange={this.onPasswordChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+        <p className={styles.navigation}>
+          Need an account? <Link to="/user/register">Sign Up</Link>
+        </p>
+      </form>
+    );
+  }
+}
+
+export default Login;
---
+++ src/components/Register/Register.js
@@ -0,0 +1,106 @@
+import React, { Component } from "react";
+import { IconContext } from "react-icons";
+import { FaUserPlus } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+import styles from "./register.module.scss";
+
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: null,
+      username: null,
+      password: null,
+      passwordConfirm: null,
+      errorMessages: []
+    };
+  }
+
+  onEmailChange = event => {
+    this.setState({ ...this.state, email: event.target.value });
+  };
+
+  onUsernameChange = event => {
+    this.setState({ ...this.state, username: event.target.value });
+  };
+
+  onPasswordChange = event => {
+    this.setState({ ...this.state, password: event.target.value });
+  };
+
+  onPasswordConfirmChange = event => {
+    this.setState({ ...this.state, passwordConfirm: event.target.value });
+  };
+
+  onRegisterSubmit = event => {
+    console.log(this.state);
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <form className={styles.container} onSubmit={this.onRegisterSubmit}>
+        <h1 className={styles.title}>
+          <IconContext.Provider
+            value={{ color: "black", className: "global-class-name" }}
+          >
+            <FaUserPlus />
+          </IconContext.Provider>
+          Register
+        </h1>
+        <p>Errors here</p>
+        <div className={styles.inputWrapper}>
+          <label for="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter email"
+            required
+            onChange={this.onEmailChange}
+          />
+        </div>
+        <div className={styles.inputWrapper}>
+          <label for="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Enter username"
+            required
+            onChange={this.onUsernameChange}
+          />
+        </div>
+        <div className={styles.inputWrapper}>
+          <label for="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter password"
+            required
+            onChange={this.onPasswordChange}
+          />
+        </div>
+        <div className={styles.inputWrapper}>
+          <label for="password2">Confirm Password</label>
+          <input
+            type="password"
+            name="password2"
+            id="password2"
+            placeholder="Re-type password"
+            required
+            onChange={this.onPasswordConfirmChange}
+          />
+        </div>
+        <button type="submit">Register</button>
+        <p className={styles.navigation}>
+          Have an account? <Link to="/user/login">Sign In</Link>
+        </p>
+      </form>
+    );
+  }
+}
+
+export default Register;
