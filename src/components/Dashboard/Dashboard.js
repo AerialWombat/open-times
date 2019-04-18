55f8496339b3ea6fce20a24e98ad27412dffe91b
AerialWombat <adrianzhan@gmail.com>
2019-04-18 15:58:03 -0400

    placeholder dashboard page

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import styles from './dashboard.module.scss';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Groups and things</p>
+    </div>
+  );
+};
+
+export default Dashboard;
