275a1a5bd8a5355e80b5fc14c841550cfa55e883
AerialWombat <adrianzhan@gmail.com>
2019-04-23 12:06:31 -0400

    Removed test data. Connected to back-end route

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,46 +3,22 @@ import GroupCard from './GroupCard/GroupCard';
 
 import styles from './dashboard.module.scss';
 
-const testGroupData = [
-  {
-    name: 'Dingos',
-    creator: 'Wombat',
-    id: '123asd',
-    timezone: 'America/New_York',
-    location: 'Discord',
-    description:
-      'Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime.',
-    members: ['Seeker', 'Mikey', 'Makila', 'Leets', 'Pebol', 'Adolin', 'Perry']
-  },
-  {
-    name: 'Yurigawa',
-    creator: 'Penukonda',
-    id: '1239-023194kfkf2',
-    timezone: 'America/New_York',
-    location: 'Skype',
-    description:
-      "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you.",
-    members: ['Gouken', 'Roana', 'Benkei', 'Sendo', 'Sori']
-  },
-  {
-    name: 'Capcom Pro Tour',
-    creator: 'Capcom',
-    id: '1239-02935sd-fi-234',
-    timezone: 'America/Detroit',
-    location: "Joe's Crabs Shacks",
-    description:
-      'Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it?',
-    members: ['Tokido', 'Daigo', 'Momochi', 'Angrybird', 'JustinWong']
-  }
-];
-
 class Dashboard extends Component {
   constructor() {
     super();
     this.state = {
-      showMembers: false
+      groups: []
     };
   }
+
+  componentDidMount = () => {
+    fetch('http://localhost:5000/api/groups/dashboard', {
+      method: 'GET',
+      credentials: 'include'
+    })
+      .then(response => response.json())
+      .then(data => this.setState({ groups: data }));
+  };
 
   toggleMemberListVisibility = () => {
     this.setState({ showMembers: !this.state.showMembers });
@@ -52,7 +28,7 @@ class Dashboard extends Component {
     return (
       <div className={styles.container}>
         <h1>Your Groups</h1>
-        {testGroupData.map(group => {
+        {this.state.groups.map(group => {
           return <GroupCard group={group} />;
         })}
       </div>
