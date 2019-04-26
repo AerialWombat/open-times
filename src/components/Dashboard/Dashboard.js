e397f5797656404eb3b0d36e543d6f99602b656f
AerialWombat <adrianzhan@gmail.com>
2019-04-26 16:47:07 -0400

    implemented groups dashboard and cards

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import Alert from '../../shared-components/Alert/Alert';
 import GroupCard from './GroupCard/GroupCard';
-// import { Link } from 'react-router-dom';
 import { FaPlusCircle } from 'react-icons/fa';
 import { IconContext } from 'react-icons';
 
@@ -11,7 +11,11 @@ class Dashboard extends Component {
     super();
     this.state = {
       groups: [],
-      showModal: false
+      showModal: false,
+      name: null,
+      location: null,
+      description: null,
+      alerts: []
     };
   }
 
@@ -19,13 +23,82 @@ class Dashboard extends Component {
     fetch('http://localhost:5000/api/groups/dashboard', {
       method: 'GET',
       credentials: 'include'
-    })
-      .then(response => response.json())
-      .then(data => this.setState({ groups: data }));
+    }).then(response =>
+      response.json().then(data => {
+        // Check for successful response
+        if (response.status === 200) {
+          this.setState({ groups: data });
+        } else {
+          // Append alerts from unsuccessful response
+          this.setState({ ...this.state, alerts: data.alerts });
+        }
+      })
+    );
   };
 
-  toggleModalVisibility = () => {
+  toggleModalVisibility = event => {
+    event.preventDefault();
     this.setState({ ...this.state, showModal: !this.state.showModal });
+  };
+
+  onInputChange = event => {
+    const { value, name } = event.target;
+    this.setState({ ...this.state, [name]: value });
+  };
+
+  // Checks for group data and then renders cards
+  getGroupCards = groups => {
+    if (groups.length > 0) {
+      return this.state.groups.map((group, index) => {
+        return <GroupCard key={index} group={group} />;
+      });
+    } else {
+      return;
+    }
+  };
+
+  onCreateSubmit = event => {
+    event.preventDefault();
+    const { name, location, description } = this.state;
+    fetch('http://localhost:5000/api/groups/create', {
+      method: 'POST',
+      credentials: 'include',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name: name,
+        location: location,
+        description: description
+      })
+    }).then(response => {
+      response
+        .json()
+        .then(data => {
+          // Check for successful response
+          if (response.status === 200) {
+            // Redirect to schedule view page for group with returned slug
+            this.props.history.push({
+              pathname: `/groups/${data.slug}`,
+              state: { success: true, message: 'Group Created!' }
+            });
+          } else {
+            // Append alerts from unsuccessful response
+            this.setState({ ...this.state, alerts: data.alerts });
+          }
+        })
+        .catch(error => console.log(error));
+    });
+  };
+
+  // Check for alerts and display Alert component for each
+  getAlertList = alerts => {
+    if (alerts.length <= 0 || !alerts[0]) {
+      return;
+    }
+    return alerts.map((alert, index) => {
+      return (
+        <Alert key={index} success={alert.success} message={alert.message} />
+      );
+    });
   };
 
   render() {
@@ -38,8 +111,9 @@ class Dashboard extends Component {
               : styles.hideModalOverlay
           }
         >
-          <form className={styles.creationModal}>
+          <form className={styles.creationModal} onSubmit={this.onCreateSubmit}>
             <h1 className={styles.title}>Create Group</h1>
+            {this.getAlertList(this.state.alerts)}
             <div className={styles.inputWrapper}>
               <label htmlFor='name'>Name</label>
               <input
@@ -63,12 +137,11 @@ class Dashboard extends Component {
             </div>
             <div className={styles.inputWrapper}>
               <label htmlFor='description'>Description</label>
-              <input
+              <textarea
                 type='text'
                 name='description'
                 id='description'
                 placeholder='Enter description (optional)'
-                required
                 onChange={this.onInputChange}
               />
             </div>
@@ -95,9 +168,7 @@ class Dashboard extends Component {
           </button>
         </header>
 
-        {this.state.groups.map((group, index) => {
-          return <GroupCard key={index} group={group} />;
-        })}
+        {this.getGroupCards(this.state.groups)}
       </div>
     );
   }
--- src/components/Dashboard/GroupCard/GroupCard.js
+++ src/components/Dashboard/GroupCard/GroupCard.js
@@ -17,47 +17,53 @@ class GroupCard extends Component {
 
   render() {
     const group = this.props.group;
-    return (
-      <div className={styles.container}>
-        <div className={styles.groupInfo}>
-          <h1 className={styles.name}>{group.name}</h1>
-          <h2 className={styles.subheader}>
-            Created by: <span>{group.creator}</span>
-          </h2>
-          <p className={styles.description}>{group.description}</p>
+
+    // Check for non-null group data
+    if (group) {
+      return (
+        <div className={styles.container}>
+          <div className={styles.groupInfo}>
+            <h1 className={styles.name}>{group.name}</h1>
+            <h2 className={styles.subheader}>
+              Created by: <span>{group.creator}</span>
+            </h2>
+            <p className={styles.description}>{group.description}</p>
+          </div>
+          <div className={styles.members}>
+            <button
+              className={styles.membersToggle}
+              onClick={this.toggleMemberListVisibility}
+            >
+              Show/Hide members...
+            </button>
+            <ul
+              className={
+                this.state.showMembers
+                  ? styles.showMemberList
+                  : styles.hideMemberList
+              }
+            >
+              {group.members.map((member, index) => {
+                return <li key={index}>{member}</li>;
+              })}
+            </ul>
+          </div>
+          <div className={styles.actions}>
+            <Link to={`/groups/:${group.slug}`} className={styles.navButton}>
+              View
+            </Link>
+            <Link
+              to={`/groups/${group.slug}/manage`}
+              className={styles.navButton}
+            >
+              Manage
+            </Link>
+          </div>
         </div>
-        <div className={styles.members}>
-          <button
-            className={styles.membersToggle}
-            onClick={this.toggleMemberListVisibility}
-          >
-            Show/Hide members...
-          </button>
-          <ul
-            className={
-              this.state.showMembers
-                ? styles.showMemberList
-                : styles.hideMemberList
-            }
-          >
-            {group.members.map((member, index) => {
-              return <li key={index}>{member}</li>;
-            })}
-          </ul>
-        </div>
-        <div className={styles.actions}>
-          <Link to={`/groups/:${group.slug}`} className={styles.navButton}>
-            View
-          </Link>
-          <Link
-            to={`/groups/${group.slug}/manage`}
-            className={styles.navButton}
-          >
-            Manage
-          </Link>
-        </div>
-      </div>
-    );
+      );
+    } else {
+      return null;
+    }
   }
 }
 
