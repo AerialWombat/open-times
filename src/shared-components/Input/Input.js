3b97999e6345841b5679e767daf64d9635a26ce0
AerialWombat <adrianzhan@gmail.com>
2019-05-21 16:04:27 -0400

    enable default value support

--- src/shared-components/Input/Input.js
+++ src/shared-components/Input/Input.js
@@ -7,11 +7,10 @@ const Input = props => {
     <div className={styles['form-group']}>
       <label htmlFor={props.name}>{props.title}</label>
       <input
-        className={styles['form__input']}
         type={props.type}
         id={props.name}
         name={props.name}
-        // value={props.value}
+        value={props.value}
         placeholder={props.placeholder}
         required={props.required}
         onChange={props.onChangeHandle}
